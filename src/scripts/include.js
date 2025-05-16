ed6c138e0d209fe59331863c45b0419ff9691c85
PaulaDelgado <pdelgad99203@universidadean.edu.co>
2025-05-15 20:15:11 -0500

    SRC-7

--- src/scripts/include.js
+++ src/scripts/include.js
@@ -1,18 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const currentPage = window.location.pathname.split("/").pop(); // obtiene el nombre del archivo actual
+
+  // Determina si mostrar íconos de usuario y notificaciones
+  const showUserIcons = !["login.html", "createaccount.html"].includes(
+    currentPage
+  );
+
   document.getElementById("navbar").innerHTML = `
     <nav class="navbar navbar-dark navbar-custom p-3 d-flex justify-content-between">
-      <a href="./dashboard.html" class="text-decoration-none">
+      <a href=${
+        !showUserIcons ? "./login.html" : "./dashboard.html"
+      } class="text-decoration-none">
         <div class="navbar-brand d-flex align-items-center gap-2">
           <img src="../../assets/img/logo.png" alt="Logo" height="30">
           <span class="text-white fw-bold">EANet</span>
+          
         </div>
       </a>
-      <div class="d-flex align-items-center gap-3">
-        <i class="bi bi-bell-fill fs-4 text-white"></i>
-        <a href="profile.html">
-          <i class="bi bi-person-circle fs-4 text-white"></i>
-        </a>
-      </div>
+      ${
+        showUserIcons
+          ? `
+        <div class="d-flex align-items-center gap-3">
+          <i class="bi bi-bell-fill fs-4 text-white"></i>
+          <div class="dropdown">
+            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="bi bi-person-circle fs-4"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end text-small" aria-labelledby="dropdownUser">
+              <li><a class="dropdown-item" href="profile.html">Perfil</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="./login.html">Cerrar sesión</a></li>
+            </ul>
+          </div>
+        </div>
+      `
+          : ""
+      }
     </nav>
   `;
 
